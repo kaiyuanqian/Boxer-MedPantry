0e2f2c1d292a5303f7bdd446d3a6c5b251112cfe
Kaiyuan Qian <116858989+kaiyuanqian@users.noreply.github.com>
2025-05-14 15:08:53 +1000

    returning cleaned order information

--- mern/server/routes/order.js
+++ mern/server/routes/order.js
@@ -29,11 +29,41 @@ const client = new shopify.clients.Rest({
 // The router will be added as a middleware and will take control of requests starting with path /record.
 const router = express.Router();
 
-// This section will help you get a list of all the orders.
+// This section will help you get a list of all the orders, with cleaned information.
 router.get("/", async (req, res) => {
     const results = await client.get({ path: 'orders' });
-    res.send(results).status(200);
+    const ordersNode = results.body.orders;
+
+    const cleanedOrders = [];
+
+    for (const order of ordersNode) {
+        console.log(order.id);
+
+        const cleanedOrder = {};
+        cleanedOrder.id = order.id;
+
+        const lineItemsNode = order.line_items;
+        const cleanedLineItems = [];
+
+        for (const lineItem of lineItemsNode) {
+            console.log(lineItem.name);
+            const cleanedLineItem = {};
+            cleanedLineItem.name = lineItem.name;
+            cleanedLineItem.sku = lineItem.sku;
+            cleanedLineItem.quantity = lineItem.quantity;
+            cleanedLineItems.push(cleanedLineItem);
+        }
+        
+        cleanedOrder.line_items = cleanedLineItems;
+        cleanedOrders.push(cleanedOrder);
+    }
+
+    res.send(cleanedOrders).status(200);
 
 });
+
+// taking an order will close the order in Shopify, preventing it from showing up in the list of orders
+
+// if the order taking fails/is cancelled, the closed order is re-opened
 
 export default router;
